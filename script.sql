7a06caa9f692cb3a45fb9d84e7766195577d8a5c
Rodricode21 <kiradex21@gmail.com>
2023-07-29 12:59:11 -0600

    add fixture data to the script.sql init file

--- script.sql
+++ script.sql
@@ -1,5 +1,8 @@
+-- Create the new schema
+CREATE SCHEMA IF NOT EXISTS movies;
+
 -- Script para crear la tabla "películas"
-CREATE TABLE movie (
+CREATE TABLE IF NOT EXISTS movies.movie (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   budget INTEGER NOT NULL,
@@ -8,7 +11,25 @@ CREATE TABLE movie (
 );
 
 -- Script para crear la tabla "opciones"
-CREATE TABLE option (
+CREATE TABLE IF NOT EXISTS movies.option (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
+
+
+-- CREATING FIXTURES DATA
+
+-- Insert initial records into the "movie" table
+INSERT INTO movies.movie (name, budget, date, duration) VALUES
+    ('Titanic', 5000, '9/16/1997', 2),
+    ('Avengers', 6000, '9/16/2012', 4),
+    ('Jurasic Park', 7000, '9/16/2001', 6),
+    ('The Incredibles', 5000, '9/16/2004', 2),
+    ('Stuart Little', 2023, '5/20/2000', 1)
+    -- Add more movie records as needed
+
+-- Insert initial records into the "option" table
+INSERT INTO movies.option (name, value) VALUES
+    ('Option 1', 'Value 1'),
+    ('Option 2', 'Value 2')
+    -- Add more option records as needed
