2d6ca88b20dae54c9e6e193239a17b815aafe081
rodricode21 <kiradex21@gmail.com>
2023-07-28 23:56:45 -0600

    load environment variables to running app

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const express = require('express');
+const express = require("express");
 var cors = require("cors");
-const moviesController = require('./controllers/moviesController');
-const optionsController = require('./controllers/optionsController');
+const moviesController = require("./controllers/moviesController");
+const optionsController = require("./controllers/optionsController");
 const app = express();
-const port = 3010; 
+const port = 3010;
+
+// load environment variables
+const dotenv = require("dotenv");
+dotenv.config();
 
 const corsOpts = {
-  origin: '*',
-  methods: [
-    'GET',
-    'POST',
-  ],
-  allowedHeaders: [
-    'Content-Type',
-  ],
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
 };
 
 app.use(cors(corsOpts));
@@ -21,10 +20,11 @@ app.use(cors(corsOpts));
 app.use(express.json());
 
 // Rutas para películas
-app.post('/api/movies', moviesController.createMovie);
+app.post("/api/movies", moviesController.createMovie);
 
 // Rutas para opciones
-app.get('/api/options', optionsController.getOptions);
+app.get("/api/options", optionsController.getOptions);
+// app.get("/api/movies", moviesController.getMovies);
 
 app.listen(port, () => {
   console.log(`API iniciada en http://localhost:${port}`);
