9967aa82650bf2a418a925f9340eb0d1889a83e3
rodricode21 <kiradex21@gmail.com>
2023-07-28 23:57:17 -0600

    refactor query to [schema][table]

--- models/movies.js
+++ models/movies.js
@@ -1,8 +1,9 @@
-const pool = require('../db');
+const pool = require("../db");
 
 const createMovie = async (movie) => {
   const { name, budget, date, duration } = movie;
-  const query = 'INSERT INTO movie (name, budget, date, duration) VALUES ($1, $2, $3, $4) RETURNING *';
+  const query =
+    "INSERT INTO movies.movie (name, budget, date, duration) VALUES ($1, $2, $3, $4) RETURNING *";
   const values = [name, budget, date, duration];
   const { rows } = await pool.query(query, values);
   return rows[0];
