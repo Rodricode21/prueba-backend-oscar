5e9ebb104212f282202db921427619300dd33082
Rodricode21 <kiradex21@gmail.com>
2023-07-29 11:51:26 -0600

    add schema to query

--- models/options.js
+++ models/options.js
@@ -1,7 +1,7 @@
 const pool = require('../db');
 
 const getOptions = async () => {
-  const query = 'SELECT * FROM option';
+  const query = 'SELECT * FROM movies.option';
   const { rows } = await pool.query(query);
   return rows;
 };
