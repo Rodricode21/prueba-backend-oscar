f855024e1f2ec13151161e7561e717adb0ae96b4
Rodricode21 <kiradex21@gmail.com>
2023-07-29 23:42:48 -0600

    feat: add pagination to movies controller

--- models/movies.js
+++ models/movies.js
@@ -9,12 +9,36 @@ const createMovie = async (movie) => {
   return rows[0];
 };
 
-const getMovies = async () => {
-  // TODO: implement pagination
-  const query = 'SELECT * FROM movies.movie'
-  const { rows } = await pool.query(query);
+const getMovies = async (req) => {
+  const { page, limit } = req.query || {} // avoid runtime error if query was undefined
+  
+  const pageNumber = parseInt(page, 10);
+  const limitNumber = parseInt(limit, 10);
 
-  return rows;
+  // Calculate the OFFSET and LIMIT values
+  const offset = (pageNumber - 1) * limitNumber;
+
+  // Execute the SQL query to get the total count
+  const countQuery = `SELECT COUNT(*) FROM movies.movie`;
+  const countResult = await pool.query(countQuery);
+  const totalCount = parseInt(countResult.rows[0].count, 10);
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(totalCount / limitNumber);
+
+  // Execute the SQL query with OFFSET and LIMIT clauses
+  const query = `SELECT * FROM movies.movie ORDER BY id LIMIT $1 OFFSET $2`;
+  const values = [limitNumber, offset];
+
+  const { rows } = await pool.query(query, values);
+
+  // Send the paginated data along with pagination metadata
+  return {
+    movies: rows,
+    totalPages,
+    currentPage: pageNumber,
+    totalItems: totalCount,
+  }
 }
 
 module.exports = {
