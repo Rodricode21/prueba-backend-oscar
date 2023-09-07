5c5b7c5363e7a62ec45e29ede2421f02d50cf18d
fontoful <kiradex21@gmail.com>
2023-09-07 00:23:35 -0600

    Revert "mock data - test controller"
    This reverts commit 749ab8b3c7e265476b0d1a565105e8428e15f732.

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use(express.json());
 // Rutas para películas
 app.post("/api/movies", moviesController.createMovie);
 app.get("/api/movies", moviesController.getMovies);
-app.get('/api/test', moviesController.testController);
 
 // Rutas para opciones
 app.get("/api/options", optionsController.getOptions);
